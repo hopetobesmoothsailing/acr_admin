6aedaa099db67f8e33b32db3d47d209eb5ed0df3
wangyi85 <jackywang8911@gmail.com>
2024-01-19 17:17:28 +0800

    add initialization to auth context

--- src/routes/hooks/use-auth.jsx
+++ src/routes/hooks/use-auth.jsx
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import {useMemo, useContext, useReducer, createContext} from "react";
+import {useRef, useMemo, useEffect, useContext, useReducer, createContext} from "react";
 
 const initialState = {
     isAuthenticated: false,
@@ -7,11 +7,31 @@ const initialState = {
 };
 
 const HANDLERS = {
+    INITIALIZE: 'INITIALIZE',
     SIGN_IN: 'SIGN_IN',
     SIGN_OUT: 'SIGN_OUT'
 };
 
 const handlers = {
+    [HANDLERS.INITIALIZE]: (state, action) => {
+        const user = action.payload;
+
+        return {
+            ...state,
+            ...(
+                // if payload (user) is provided, then is authenticated
+                user
+                    ? ({
+                        isAuthenticated: true,
+                        isLoading: false,
+                        user
+                    })
+                    : ({
+                        isLoading: false
+                    })
+            )
+        };
+    },
     [HANDLERS.SIGN_IN]: (state, action) => {
         const user = action.payload;
 
@@ -37,6 +57,50 @@ const AuthContext = createContext({undefined});
 export const AuthProvider = (props) => {
     const {children} = props;
     const [state, dispatch] = useReducer(reducer, initialState);
+    const initialized = useRef(false);
+
+    const initialize = async () => {
+        // Prevent from calling twice in development mode with React.StrictMode enabled
+        if (initialized.current) {
+            return;
+        }
+
+        initialized.current = true;
+
+        let isAuthenticated = false;
+
+        try {
+            isAuthenticated = window.sessionStorage.getItem('authenticated') === 'true';
+        } catch (err) {
+            console.error(err);
+        }
+
+        if (isAuthenticated) {
+            const user = {
+                id: '5e86809283e28b96d2d38537',
+                avatar: '/assets/avatars/avatar-anika-visser.png',
+                name: 'Anika Visser',
+                email: 'anika.visser@devias.io'
+            };
+
+            dispatch({
+                type: HANDLERS.INITIALIZE,
+                payload: user
+            });
+        } else {
+            dispatch({
+                type: HANDLERS.INITIALIZE
+            });
+        }
+    };
+
+    useEffect(
+        () => {
+            initialize();
+        },
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        []
+    );
 
     const signIn = async (user) => {
         try {
